847dab58143b8ef4dfc319bc7bd8b5b8c3b4f350
david-eom <david.eom.hyo.shik@gmail.com>
2021-06-04 00:32:35 +0800

    Reformat CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,34 +4,32 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //  target to follow in Uuity Inspector -> set to player car
+    // target car to follow
     public CarController target;
 
+    // relative direction between camera and car
     private Vector3 offsetDirection;
-    //  set in Unity Inspector -> min = 15, max = 35
-    public float minDistance, maxDistance;
+
+    // to set in Unity inspector
+    public float minDistance;
+    public float maxDistance;
     private float activeDistance;
 
-    // Start is called before the first frame update
     void Start()
     {   
-        //  a constant geometric positioning difference from the car to the top-down camera at the 1st frame
         offsetDirection = transform.position - target.transform.position;
-
-        //  activeDist -> set to minDist
+        // set min distance as active distance
         activeDistance = minDistance;
-
-        //  set to unit
+        // normalize offset direction
         offsetDirection.Normalize();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        //  set the dist of top down camera to adjust according to speed
-        activeDistance = minDistance + ((maxDistance - minDistance) * (target.theRB.velocity.magnitude / target.maxSpeed));
-
-        //  constantly update the camera's geometric position to the offset
+        // adjust active distance according to speed
+        activeDistance = minDistance + ((maxDistance - minDistance) * (target.rigidBody.velocity.magnitude / target.maxSpeed));
+        // update the camera's geometric position
         transform.position = target.transform.position + (offsetDirection * activeDistance);
     }
 }
