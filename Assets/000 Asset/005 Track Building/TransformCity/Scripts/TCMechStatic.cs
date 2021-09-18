245f782a7ad62d2283135534048d74264674dcd9
Timothy Yang <62177572+Timothyoung97@users.noreply.github.com>
2021-09-18 13:18:24 +0800

    Delete Assets/000 Asset/005 Track Building/TransformCity directory
    To reduce repo storage size before downgrading the data pack to 0.

--- Assets/000 Asset/005 Track Building/TransformCity/Scripts/TCMechStatic.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//IL.ranch, ILonion32@gmail.com, 2020.
-public class TCMechStatic : MonoBehaviour
-{
-    public static bool ChangeCurrentState;
-}
--- Assets/000 Asset/005 Track Building/TransformCity/_ReadMe.txt
+++
@@ -1,7 +0,0 @@
-##########################
-Modular Transformer City.
-Documentation: https://ilonion.com/transcity_documentation
-##########################
-
-IL.ranch, 2020.
-ILonion32@gmail.com
